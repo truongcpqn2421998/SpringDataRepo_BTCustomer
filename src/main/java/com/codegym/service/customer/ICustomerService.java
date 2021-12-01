8a81eeced1ebd127bdeea7bad65faf6a2e05e8f6
truongcpqn2421998 <truongcpqn2421998@gmail.com>
2021-12-01 15:20:36 +0700

    update

--- src/main/java/com/codegym/service/customer/ICustomerService.java
+++ src/main/java/com/codegym/service/customer/ICustomerService.java
@@ -3,9 +3,15 @@ package com.codegym.service.customer;
 import com.codegym.model.Customer;
 import com.codegym.model.Province;
 import com.codegym.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface ICustomerService extends IGeneralService<Customer> {
     Iterable<Customer> findAllByProvince(Province province);
+    Page<Customer> findAll(Pageable pageable);
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
+
+
 }
